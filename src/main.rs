59ad19039e959122bc22eec672fbebf4a69a4430
danwritecode <dnelson4993@outlook.com>
2023-09-06 00:24:16 -0400

    added tests for routes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use axum::{
     routing::{get, post},
     Router,
-    response::{Html, IntoResponse}, http::{HeaderMap, header}
+    response::{Html, IntoResponse, Result, Response}, http::{HeaderMap, header, StatusCode}
 };
 
 use tower_livereload::LiveReloadLayer;
@@ -16,7 +16,7 @@ use tera::{Context, Tera};
 
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), AppError> {
     // build our application with a route
     let app = Router::new()
         .route("/", get(root_route))
@@ -30,44 +30,45 @@ async fn main() {
     println!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
-        .await
-        .unwrap();
+        .await?;
+
+    Ok(())
 }
 
-async fn css() -> impl IntoResponse {
+async fn css() -> Result<impl IntoResponse, AppError> {
     let mut headers = HeaderMap::new();
-    headers.insert(header::CONTENT_TYPE, "text/css".parse().unwrap());
-    let css = std::fs::read_to_string("dist/output.css").unwrap();
+    headers.insert(header::CONTENT_TYPE, "text/css".parse()?);
+    let css = std::fs::read_to_string("dist/output.css")?;
     
-    (headers, css)
+    Ok((headers, css))
 }
 
-async fn js() -> impl IntoResponse {
+async fn js() -> Result<impl IntoResponse, AppError> {
     let mut headers = HeaderMap::new();
-    headers.insert(header::CONTENT_TYPE, "application/javascript".parse().unwrap());
-    headers.insert(header::CACHE_CONTROL, "public, max-age=3600".parse().unwrap());
-    let js = std::fs::read_to_string("dist/output.js").unwrap();
+    headers.insert(header::CONTENT_TYPE, "application/javascript".parse()?);
+    headers.insert(header::CACHE_CONTROL, "public, max-age=3600".parse()?);
+    let js = std::fs::read_to_string("dist/output.js")?;
     
-    (headers, js)
+    Ok((headers, js))
 }
 
-async fn root_route() -> Html<String> {
+async fn root_route() -> Result<Html<String>, AppError> {
     let context = Context::new();
-    let rendered = render_with_global_context("root/index.html", &context).unwrap();
+    let rendered = render_with_global_context("root/index.html", &context)?;
     
-    Html(rendered)
+    Ok(Html(rendered))
 }
 
-async fn other_route_route() -> Html<String> {
+async fn other_route_route() -> Result<Html<String>, AppError> {
     let mut context = Context::new();
     context.insert("foo", &"hello from the other route");
-    let rendered = render_with_global_context("other-route/index.html", &context).unwrap();
+    let rendered = render_with_global_context("other-route/index.html", &context)?;
     
-    Html(rendered)
+    Ok(Html(rendered))
 }
 
-async fn clicked_uix() -> Html<String> {
-    Html("<p class=\"text-center mt-10\">Hello from htmx</p>".to_string())
+async fn clicked_uix() -> Result<Html<String>, AppError> {
+    Ok(Html("<p class=\"text-center mt-10\">Hello from htmx</p>".to_string()))
 }
 
 lazy_static! {
@@ -85,4 +86,62 @@ fn render_with_global_context(template: &str, specific_context: &Context) -> ter
 
     context.extend(specific_context.clone());
     TEMPLATES.render(template, &context)
+}
+
+#[derive(Debug)]
+struct AppError(anyhow::Error);
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {}", self.0),
+        )
+            .into_response()
+    }
+}
+
+impl<E> From<E> for AppError
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        Self(err.into())
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::{root_route, other_route_route, css, js, clicked_uix};
+
+    #[tokio::test]
+    async fn css_test() {
+        let res = css().await;
+        assert!(res.is_ok());
+    }
+
+    #[tokio::test]
+    async fn js_test() {
+        let res = js().await;
+        assert!(res.is_ok());
+    }
+
+    #[tokio::test]
+    async fn root_route_test() {
+        let res = root_route().await;
+        assert!(res.is_ok());
+    }
+
+    #[tokio::test]
+    async fn other_route_route_test() {
+        let res = other_route_route().await;
+        assert!(res.is_ok());
+    }
+
+    #[tokio::test]
+    async fn clicked_uix_test() {
+        let res = clicked_uix().await;
+        assert!(res.is_ok());
+    }
 }
